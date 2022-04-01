d7da777c9d5ee201a9dc4308822e8c1d72cf0866
Jacob <Jacob.McCann214@gmail.com>
2022-04-01 11:22:44 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function isPalindrome(string){
+  return string == string.split('').reverse().join('')
 }
 
-/* 
-  Add your pseudocode here
+
+
+
+/*
+  Create isPalindrome function
+  Parameter "argument" of string
+  return the string if string is a palindrome "mom", "racecar", etc.
+  string will loosely "==" to the another string that is flipped
 */
 
 /*
-  Add written explanation of your solution here
+  So we passed the string as a parameter,
+  returns string as true if it's comparison equals to the reveresed string:
+  So The string value is initially 'split' into an array
+  then we 'reverse' the value inside the array and lastly 'join' the array back into a nre string for our compariosn
 */
 
 // You can run `node index.js` to view these console logs
